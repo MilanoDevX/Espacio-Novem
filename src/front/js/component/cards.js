3bf80d9ef0c029b5f0e30348583241e6d19d3a11
Katerine Céspedes <katerinecesbal@hotmail.com>
2025-01-13 20:20:30 +0000

    añade front de inicio de landing page

---
+++ src/front/js/component/cards.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../../styles/home.css";
+import consultorio1 from "../../img/consultorio1.webp";
+import consultorio2 from "../../img/consultorio2.webp";
+import consultorio3 from "../../img/consultorio3.webp";
+
+export const Cards = () => {
+    return (
+        <div className=" mt-5">
+            <h3 className="text-center title-espacio fs-2 ">Nuestros consultorios</h3>
+            <div className="d-flex justify-content-center align-item-center">
+                <div className="card mx-3">
+                    <div>
+                        <img className="card-image" src={consultorio1} alt="Consultorio 1" />
+                    </div>
+                    <div className="category"> Consultorio 1 </div>
+                    <div className="heading">
+                        A heading that must span over two lines
+                    </div>
+                    <button className="btn solicitar text-light">Consultar</button>
+                </div>
+
+                <div className="card mx-3">
+                    <div>
+                        <img className="card-image" src={consultorio2} alt="Consultorio 2" />
+                    </div>
+                    <div className="category"> Consultorio 2 </div>
+                    <div className="heading">
+                        A heading that must span over two lines
+                    </div>
+                    <button className="btn solicitar text-light">Consultar</button>
+                </div>
+
+                <div className="card mx-3">
+                    <div>
+                        <img className="card-image" src={consultorio3} alt="Consultorio 3" />
+                    </div>
+                    <div className="category"> Consultorio 3 </div>
+                    <div className="heading">
+                        A heading that must span over two lines
+                    </div>
+                    <button className="btn solicitar text-light">Consultar</button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
