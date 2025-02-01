d8397264eb8063ae8e4cf26c159c34bf8d9d3fab
Fiorella-vq <fio.89@hotmail.com>
2025-02-01 18:34:06 -0300

    Vista admin

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,12 +12,13 @@ import { Header } from "./component/header";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-import {ContactMap} from "./component/contactMap";
+import { ContactMap } from "./component/contactMap";
 import { UserProfile } from "./component/userProfile";
 import { Register } from "./component/register";
 import { AboutUs } from "./component/aboutUs";
 import { SendEmail } from "./component/send-email";
 import { ResetPassword } from "./component/reset-password";
+import { Admin } from "./component/admin";
 
 
 //create your first component
@@ -25,10 +26,10 @@ const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-    
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
-    
- 
+
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+
+
 
     return (
         <div>
@@ -38,17 +39,18 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
-                        <Route element={<AboutUs/>} path="/aboutUs" />
+                        <Route element={<AboutUs />} path="/aboutUs" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<Reservations />} path="/reservations" />
                         <Route element={<SendEmail />} path="/send-email" />
                         <Route element={<ContactMap />} path="/contactMap" />
-                        <Route element={<UserProfile/>} path="/userProfile"/>
-                        <Route element={<ResetPassword/>} path="/reset-password"/>
+                        <Route element={<UserProfile />} path="/userProfile" />
+                        <Route element={<ResetPassword />} path="/reset-password" />
+                        <Route element={<Admin />} path="/admin" />
                         <Route element={<Register />} path="/register" />
                         <Route element={<Header />} path="/home" />
                         <Route element={<h1>Not found!</h1>} />
-                        </Routes>
+                    </Routes>
                     <Footer />
                 </ScrollToTop>
             </BrowserRouter>
