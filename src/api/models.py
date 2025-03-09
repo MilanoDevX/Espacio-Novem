1f851425cd221e873229c1b8f8f7f6ce4ce4c40d
Elías Milano <87205729+MilanoDevX@users.noreply.github.com>
2025-03-09 23:35:34 +0000

    corrected error in 'is_admin' column from 'User' class, in models.py

--- src/api/models.py
+++ src/api/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     telefono = db.Column(db.String(20), unique=False, nullable=False)
     reservations = db.relationship('Reservation', backref='user', lazy=True)
-    is_admin = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean(), default=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
