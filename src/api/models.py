573d34eceef76681438e3e5b6ab2055fe02be71a
Fiorella-vq <fio.89@hotmail.com>
2025-03-17 17:55:08 -0300

    Migraciones

--- src/api/models.py
+++ src/api/models.py
@@ -1,21 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False) 
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
-    telefono = db.Column(db.String(20), nullable=False) 
+    telefono = db.Column(db.String(20), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, unique=False, nullable=False)
     reservations = db.relationship('Reservation', backref='user', lazy=True)
-
     def __repr__(self):
         return f'<User {self.email}>'
-    
     def __init__(self, name, last_name, email, password, telefono, is_admin=False):
         self.name = name
         self.last_name = last_name
@@ -33,24 +29,19 @@ class User(db.Model):
             "telefono":self.telefono,
             "is_active":self.is_active,
         }
-    
-
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     hour = db.Column(db.Time, nullable=False)
     office = db.Column(db.Integer, nullable=False)
-    
     def __repr__(self):
         return f'<Reservation {self.user_id}>'
-    
     def __init__(self, user_id, date, hour, office):
         self.user_id = user_id
         self.date = date
         self.hour = hour
         self.office = office
-
     def serialize(self):
         return {
             'id': self.id,
@@ -59,3 +50,9 @@ class Reservation(db.Model):
             'hour': self.hour.strftime('%H:%M'),
             'office': self.office
         }
+
+
+
+
+
+
