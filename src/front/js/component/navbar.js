b37e26b55461832205f5f27e4cd537981252cc6f
Fiorella-vq <fio.89@hotmail.com>
2025-04-16 15:15:30 -0300

    Ajustes en validateForm en navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -90,7 +90,7 @@ export const Navbar = () => {
   const validateForm = () => {
     let valid = true;
     let validationErrors = { email: "", password: "" };
-
+  
     if (!user.email) {
       validationErrors.email = "El correo electrónico es obligatorio.";
       valid = false;
@@ -98,19 +98,16 @@ export const Navbar = () => {
       validationErrors.email = "Por favor ingresa un correo electrónico válido.";
       valid = false;
     }
-
+  
     if (!user.password) {
       validationErrors.password = "La contraseña es obligatoria.";
       valid = false;
-    } else {
-  
-      validationErrors.password = "La contraseña no es correcta.";
-      valid = false;
     }
-
+  
     setErrors(validationErrors);
     return valid;
-};
+  };
+  
 
   const loginUser = async (e) => {
     e.preventDefault();
